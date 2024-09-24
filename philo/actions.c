c67ed05e8758cf60d77361d78f4e92c2b9c69fa5
fbbot <fbbot>
2024-09-24 15:32:09 +0100

    Mandatory is broken

--- philo/actions.c
+++ philo/actions.c
@@ -6,7 +6,7 @@
 /*   By: fbbot <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 16:57:05 by fbbot             #+#    #+#             */
-/*   Updated: 2024/09/22 22:25:01 by fbbot            ###   ########.fr       */
+/*   Updated: 2024/09/24 15:30:23 by fbbot            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,25 +24,29 @@ void	grab_forks(t_philo philo, int fork1, int fork2)
 
 void	eating(t_philo *ph)
 {
-	int		fork1;
 	int		fork2;
 	t_philo	philo;
 
 	philo = *ph;
 	if (!check_death(philo, 0))
 		return ;
-	fork1 = philo.id;
 	fork2 = (philo.id % philo.setup->num_philos) + 1;
 	if (philo.id % 2 == 0)
-		grab_forks(philo, fork1, fork2);
+		grab_forks(philo, philo.id, fork2);
 	else
-		grab_forks(philo, fork2, fork1);
+	{
+		if (philo.setup->num_philos % 2 == 1 && philo.id == 1)
+			ft_usleep(philo.setup->time_eat, philo);
+		else
+			ft_usleep(1, philo);
+		grab_forks(philo, fork2, philo.id);
+	}
 	ft_printf("%ld %d is eating\n", philo);
 	pthread_mutex_lock(&philo.setup->mealock);
 	ph->last_meal = get_timestamp();
 	pthread_mutex_unlock(&philo.setup->mealock);
 	ft_usleep(philo.setup->time_eat, philo);
-	pthread_mutex_unlock(&(philo.forks[fork1 - 1]));
+	pthread_mutex_unlock(&(philo.forks[philo.id - 1]));
 	pthread_mutex_unlock(&(philo.forks[fork2 - 1]));
 }
 
@@ -72,7 +76,7 @@ void	*living(void *ph)
 		eating(philo);
 		sleeping(*philo);
 		i++;
-		usleep(500);
+		usleep(100);
 	}
 	return (NULL);
 }
